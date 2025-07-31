eb23394a3873e69b622a4a0a868351428f396bdf
Anandhakrishnan <anandhakrishnannn9@gmail.com>
2025-07-31 14:33:25 +0530

    removed privacy things

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'anandhannn1122@gmail.com'  
-app.config['MAIL_PASSWORD'] = 'wpzh udck iogx zskg'     
+app.config['MAIL_PASSWORD'] = ''     
 
 csrf = CSRFProtect(app)
 db = SQLAlchemy(app)
